03e600c8f68dbc67b17270f028bf48a12fbc0030
angryw1sp <angrywisp96@gmail.com>
2025-12-18 22:05:31 +0300

    Refactor configuration settings in config.py for consistency and clarity

--- bot/core/config.py
+++ bot/core/config.py
@@ -2,14 +2,13 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str = ''
-    BACKEND_URL: str = 'http://localhost:8000/api/v1/'
-    model_config = SettingsConfigDict(
-        env_file='.env',
-        extra='ignore'
-    )
-    database_url: str = 'sqlite+aiosqlite:///./bot.db'
-    
+    BOT_TOKEN: str = ""
+    BACKEND_URL: str = "http://localhost:8000/api/v1/"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    database_url: str = "sqlite+aiosqlite:///./bot.db"
+    habit_service_url: str = "http://localhost:8000/api/v1"
+    user_service_url: str = "http://localhost:8001/internal/users"
+    internal_token: str = "your_internal_token"
 
 
 settings = Settings()
